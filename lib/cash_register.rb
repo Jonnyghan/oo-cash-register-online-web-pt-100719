a7342870adf72f0b3598b3ac852b43b2f27ee576
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-11-19 21:11:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class CashRegister
+  attr_accessor :total, :discount, :items, :last_transaction_amount 
+  
  def initialize(discount = 0)
     @total = 0
     @discount = discount
